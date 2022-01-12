a6051b49d731a423c9ac0dc5bc65b71cb5b7fe3c
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-13 00:52:38 +0300

    adds markup for images

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector('.gallery');
+
+const imagesMarkUp = createMarkupForImages(galleryItems);
+
+console.log(imagesMarkUp);
+
+listEl.insertAdjacentHTML('beforeend', imagesMarkUp);
+
+function createMarkupForImages(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+}
