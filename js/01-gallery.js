9402a9554166dd2e34c05c9ae625636b49ab377c
Tatiana Maksimova <tat.maksimova2010@ya.ru>
2022-01-17 14:57:38 +0300

    adds basicLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,13 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+// 1. Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
 console.log(galleryItems);
 
-const listEl = document.querySelector('.gallery');
+const container = document.querySelector('.gallery');
 
 const imagesMarkUp = createMarkupForImages(galleryItems);
 
-console.log(imagesMarkUp);
-
-listEl.insertAdjacentHTML('beforeend', imagesMarkUp);
+container.insertAdjacentHTML('beforeend', imagesMarkUp);
 
 function createMarkupForImages(galleryItems) {
   return galleryItems
@@ -26,4 +24,27 @@ function createMarkupForImages(galleryItems) {
   </div>`;
     })
     .join('');
+}
+
+// 2. Реализация делегирования на div.gallery и получение url большого изображения.
+
+container.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}">`);
+  instance.show();
+}
+
+// Добавь закрытие модального окна по нажатию клавиши Escape. Сделай так, чтобы прослушивание клавиатуры было только пока открыто модальное окно. У библиотеки basicLightbox есть метод для программного закрытия модального окна.
+
+window.addEventListener('keydown', onKeyPress);
+
+function onKeyPress(evt) {
+  console.log(evt.key);
 }
